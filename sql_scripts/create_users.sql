0069f572d05ac50ee2c266baa5ffb5e61140a4ae
valezamora <valezamora@gmail.com>
2018-09-28 15:02:54 -0600

    Update create_users.sql

--- sql_scripts/create_users.sql
+++ sql_scripts/create_users.sql
@@ -17,16 +17,18 @@ primary key(id_student)
 
 create table administrative_boss(
 id_administrative_boss varchar(10) not null,
-foreign key (id_administrative_boss) references user(user_id),
-primary key(id_administrative_boss)
+primary key(id_administrative_boss),
+foreign key (id_administrative_boss) references user(user_id)
 );
 
 create table administrative_assistant(
 id_administrative_assistant varchar(10) not null,
-foreign key (id_administrative_assistant) references user(user_id),
+primary key(id_administrative_assistant),
+foreign key (id_administrative_assistant) references user(user_id)
 );
 
 create table professor(
 id_professor varchar(10) not null,
-foreign key (id_professor) references user(user_id),
+primary key(id_professor),
+foreign key (id_professor) references user(user_id)
 );
