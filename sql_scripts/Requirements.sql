09de07e2fb3685c05a17b81e8babb198558649ed
estalg <33506442+estalg@users.noreply.github.com>
2018-10-01 23:55:34 -0600

    Arreglo requirements

--- sql_scripts/Requirements.sql
+++ sql_scripts/Requirements.sql
@@ -1,14 +1,14 @@
-Create Table Requirements(
-	Requirements_Number	INT	Primary Key,
-	Description    	    VARCHAR(250)	Not Null,
-	Type		        Char			Not Null
+Create Table requirements(
+	requirements_Number	        INT	Primary Key,
+	requirements_Description    VARCHAR(250)	Not Null,
+	requirements_Type		    Char			Not Null
 );
 
-Create Table States_Requirements(
-	Requirements_Number	INT 	Not Null,
-	Request_Number		INT	    Not Null,
-	State		        Bool	Not Null Default 0,
-	FOREIGN KEY (Requirements_Number) References Requirements(Requirements_Number),
-	FOREIGN KEY (Request_Number) References Request(Request_Number),
-	PRIMARY KEY (Requirements_Number, Request_Number)
+Create Table states_requirements(
+	requirements_Number	INT 	Not Null,
+	request_Number		INT	    Not Null,
+	request_State		        Bool	Not Null Default 0,
+	FOREIGN KEY (requirements_Number) References requirements(requirements_Number),
+	FOREIGN KEY (request_Number) References Request(request_Number),
+	PRIMARY KEY (requirements_Number, request_Number)
 );
