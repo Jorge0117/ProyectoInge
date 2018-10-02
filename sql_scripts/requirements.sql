322a95ad97c7dfd35428bc2339450fbcd063de30
estalg <33506442+estalg@users.noreply.github.com>
2018-10-01 23:59:44 -0600

    Update requirements.sql

--- sql_scripts/requirements.sql
+++ sql_scripts/requirements.sql
@@ -1,14 +1,14 @@
 Create Table requirements(
-	requirements_Number	        INT	Primary Key,
-	requirements_Description    VARCHAR(250)	Not Null,
-	requirements_Type		    Char			Not Null
+	requirements_number	        INT	Primary Key,
+	requirements_description    VARCHAR(250)	Not Null,
+	requirements_type		    Char			Not Null
 );
 
 Create Table states_requirements(
-	requirements_Number	INT 	Not Null,
-	request_Number		INT	    Not Null,
-	request_State		        Bool	Not Null Default 0,
-	FOREIGN KEY (requirements_Number) References requirements(requirements_Number),
-	FOREIGN KEY (request_Number) References Request(request_Number),
-	PRIMARY KEY (requirements_Number, request_Number)
+	requirements_number	INT 	Not Null,
+	request_number		INT	    Not Null,
+	request_state		        Bool	Not Null Default 0,
+	FOREIGN KEY (requirements_number) References requirements(requirements_number),
+	FOREIGN KEY (request_number) References Request(request_number),
+	PRIMARY KEY (requirements_number, request_number)
 );
