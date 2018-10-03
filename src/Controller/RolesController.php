b7bbaea6e50f39a66855d943cb3afda765c21cd7
Kevin Josue Jiménez Álvarez <kevinja9608@gmail.com>
2018-10-03 16:01:08 -0600

    Baking done

---
+++ src/Controller/RolesController.php
@@ -0,0 +1,109 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Roles Controller
+ *
+ * @property \App\Model\Table\RolesTable $Roles
+ *
+ * @method \App\Model\Entity\Role[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class RolesController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
+    public function index()
+    {
+        $roles = $this->paginate($this->Roles);
+
+        $this->set(compact('roles'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Role id.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $role = $this->Roles->get($id, [
+            'contain' => ['Permissions']
+        ]);
+
+        $this->set('role', $role);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $role = $this->Roles->newEntity();
+        if ($this->request->is('post')) {
+            $role = $this->Roles->patchEntity($role, $this->request->getData());
+            if ($this->Roles->save($role)) {
+                $this->Flash->success(__('The role has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The role could not be saved. Please, try again.'));
+        }
+        $permissions = $this->Roles->Permissions->find('list', ['limit' => 200]);
+        $this->set(compact('role', 'permissions'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Role id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $role = $this->Roles->get($id, [
+            'contain' => ['Permissions']
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $role = $this->Roles->patchEntity($role, $this->request->getData());
+            if ($this->Roles->save($role)) {
+                $this->Flash->success(__('The role has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The role could not be saved. Please, try again.'));
+        }
+        $permissions = $this->Roles->Permissions->find('list', ['limit' => 200]);
+        $this->set(compact('role', 'permissions'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Role id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $role = $this->Roles->get($id);
+        if ($this->Roles->delete($role)) {
+            $this->Flash->success(__('The role has been deleted.'));
+        } else {
+            $this->Flash->error(__('The role could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
