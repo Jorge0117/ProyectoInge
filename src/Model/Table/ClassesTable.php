0ee31bd77d1ff8a4f339d48c585db2b004694081
Unknown <jorgea1177@gmail.com>
2018-10-03 10:20:19 -0600

    baking models

---
+++ src/Model/Table/ClassesTable.php
@@ -0,0 +1,89 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Classes Model
+ *
+ * @property \App\Model\Table\CoursesTable|\Cake\ORM\Association\BelongsTo $Courses
+ * @property \App\Model\Table\ProfessorsTable|\Cake\ORM\Association\BelongsTo $Professors
+ *
+ * @method \App\Model\Entity\Class get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Class newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Class[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Class|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Class|bool saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Class patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Class[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Class findOrCreate($search, callable $callback = null, $options = [])
+ */
+class ClassesTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('classes');
+        $this->setDisplayField('course_id');
+        $this->setPrimaryKey(['course_id', 'class_number', 'semester', 'year']);
+
+        $this->belongsTo('Courses', [
+            'foreignKey' => 'course_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('Professors', [
+            'foreignKey' => 'professor_id'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->allowEmpty('class_number', 'create');
+
+        $validator
+            ->allowEmpty('semester', 'create');
+
+        $validator
+            ->scalar('year')
+            ->allowEmpty('year', 'create');
+
+        $validator
+            ->boolean('state')
+            ->allowEmpty('state');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['course_id'], 'Courses'));
+        $rules->add($rules->existsIn(['professor_id'], 'Professors'));
+
+        return $rules;
+    }
+}
