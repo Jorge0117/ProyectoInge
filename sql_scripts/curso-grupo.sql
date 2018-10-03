c607682e54ab17c1a134b281392ba39d6098d315
Joseph Rementería <rementeriajoseph@gmail.com>
2018-10-03 09:56:38 -0600

    A view was added to the 'cursos-grupo' scripts

--- sql_scripts/curso-grupo.sql
+++ sql_scripts/curso-grupo.sql
@@ -15,3 +15,9 @@ create table classes (
     foreign key(course_id) references courses(code)
 )
 
+CREATE VIEW courses_classes_vw AS
+	SELECT CR.code AS 'Sigla', CR.name AS 'Curso', CL.class_number AS 'Grupo', U.name + U.lastname1 +U.lastname2 AS 'Profesor', CL.semester AS 'Semestre', CL.year AS 'Año'
+    FROM courses CR, classes CL, professors P, users as U
+    WHERE CR.code = CL.course_id
+    AND P.user_id = CL.professor_id
+    AND P.user_id = U.identification_number
