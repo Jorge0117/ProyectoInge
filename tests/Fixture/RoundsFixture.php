d8fd55573c00e70dac0f279a31a548f51350926a
Daniel <110100010111h@gmail.com>
2018-10-12 10:54:29 -0600

    Arregla rounds según a la petición de la profe
    Petición: Mostrar calendarios en las casillas de fechas, una sola ronda es mostrada, botones al lado.
    
    TODO: fix botones, arreglar agregar ronda con post

--- tests/Fixture/RoundsFixture.php
+++ tests/Fixture/RoundsFixture.php
@@ -17,12 +17,11 @@ class RoundsFixture extends TestFixture
      */
     // @codingStandardsIgnoreStart
     public $fields = [
+        'start_date' => ['type' => 'date', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'round_number' => ['type' => 'string', 'length' => null, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
         'semester' => ['type' => 'string', 'length' => null, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
         'year' => ['type' => 'string', 'length' => null, 'null' => false, 'default' => null, 'collate' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
-        'start_date' => ['type' => 'date', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'end_date' => ['type' => 'date', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
-        'approve_limit_date' => ['type' => 'date', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['start_date'], 'length' => []],
         ],
@@ -42,12 +41,11 @@ class RoundsFixture extends TestFixture
     {
         $this->records = [
             [
+                'start_date' => '2018-10-09',
                 'round_number' => 'Lorem ipsum dolor sit amet',
                 'semester' => 'Lorem ipsum dolor sit amet',
                 'year' => 'Lorem ipsum dolor sit amet',
-                'start_date' => '2018-10-05',
-                'end_date' => '2018-10-05',
-                'approve_limit_date' => '2018-10-05'
+                'end_date' => '2018-10-09'
             ],
         ];
         parent::init();
