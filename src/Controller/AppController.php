5e7827750884c9ec09d0b3c848d413de229d0625
Joseph Rementería <rementeriajoseph@gmail.com>
2018-11-02 13:19:42 -0600

    Correcciones merge CorrePb a development

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -113,13 +113,13 @@ class AppController extends Controller
 
         return true;
 
-        $role_c = new RolesController;
-        $action =$this->request->getParam('action');
-        $module = $this->request->getParam('controller');
+        //$role_c = new RolesController;
+        //$action =$this->request->getParam('action');
+        //$module = $this->request->getParam('controller');
         //echo($action);
         //echo($module);
-		return true;
-        return $role_c->is_Authorized($user['role_id'], $module, $action);*/
-        return true;
+		//return true;
+        //return $role_c->is_Authorized($user['role_id'], $module, $action);
+        //return true;
     }
 }
