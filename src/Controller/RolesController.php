4d1ce7ee91d50aec07e497fd09561091f0693217
Kevin Josue Jiménez Álvarez <kevinja9608@gmail.com>
2018-11-04 01:02:11 -0600

    Crear metodo getPermissions

--- src/Controller/RolesController.php
+++ src/Controller/RolesController.php
@@ -28,76 +28,57 @@ class RolesController extends AppController
      */
     public function edit()
     {
+        $this->loadModel('Permissions');
+
         $n_permission_types = count($this->permission_types);
         $this->set(compact('n_permission_types'));
-
-        $roles = $this->paginate($this->Roles);
-
-        $this->set(compact('roles'));
 
         $roles_array = $this->Roles->find('list');
         $this->set(compact('roles_array'));
 
-        $this->loadModel('Permissions');
-
         //Administrator permissions
-        $administrator_permissions = $this->Permissions->find('list')->matching('Roles', function ($q) {
-            return $q->where(['Roles.role_id' => 'Administrador']);
-        })->toArray();
-        $this->set(compact('administrator_permissions'));
-
+        $administrator_permissions = $this->Permissions->getPermissions('Administrador');
         for ($i = 0; $i < $n_permission_types; $i++) {
             $administrator_permissions_matrix[$i][0] = $this->permission_types[$i];
             for ($j = 1; $j <= count($this->permissions_id_matrix[$i]); $j++) {
                 $administrator_permissions_matrix[$i][$j] = in_array($this->permissions_id_matrix[$i][$j - 1], $administrator_permissions);
             }
-        }
-        
+        }    
+        $this->set(compact('administrator_permissions'));
         $this->set(compact('administrator_permissions_matrix'));
+        debug($administrator_permissions);
 
         //Assistant permissions
-        $assistant_permissions = $this->Permissions->find('list')->matching('Roles', function ($q) {
-            return $q->where(['Roles.role_id' => 'Asistente']);
-        })->toArray();
-        $this->set(compact('assistant_permissions'));
-
+        $assistant_permissions = $this->Permissions->getPermissions('Asistente');
         for ($i = 0; $i < $n_permission_types; $i++) {
             $assistant_permissions_matrix[$i][0] = $this->permission_types[$i];
             for ($j = 1; $j <= count($this->permissions_id_matrix[$i]); $j++) {
                 $assistant_permissions_matrix[$i][$j] = in_array($this->permissions_id_matrix[$i][$j - 1], $assistant_permissions);
             }
         }
-
+        $this->set(compact('assistant_permissions'));
         $this->set(compact('assistant_permissions_matrix'));
 
         //Student permissions
-        $student_permissions = $this->Permissions->find('list')->matching('Roles', function ($q) {
-            return $q->where(['Roles.role_id' => 'Estudiante']);
-        })->toArray();
-        $this->set(compact('student_permissions'));
-
+        $student_permissions = $this->Permissions->getPermissions('Estudiante');
         for ($i = 0; $i < $n_permission_types; $i++) {
             $student_permissions_matrix[$i][0] = $this->permission_types[$i];
             for ($j = 1; $j <= count($this->permissions_id_matrix[$i]); $j++) {
                 $student_permissions_matrix[$i][$j] = in_array($this->permissions_id_matrix[$i][$j - 1], $student_permissions);
             }
         }
-
+        $this->set(compact('student_permissions'));
         $this->set(compact('student_permissions_matrix'));
 
         //Professor permissions
-        $professor_permissions = $this->Permissions->find('list')->matching('Roles', function ($q) {
-            return $q->where(['Roles.role_id' => 'Profesor']);
-        })->toArray();
-        $this->set(compact('professor_permissions'));
-
+        $professor_permissions = $this->Permissions->getPermissions('Profesor');
         for ($i = 0; $i < $n_permission_types; $i++) {
             $professor_permissions_matrix[$i][0] = $this->permission_types[$i];
             for ($j = 1; $j <= count($this->permissions_id_matrix[$i]); $j++) {
                 $professor_permissions_matrix[$i][$j] = in_array($this->permissions_id_matrix[$i][$j - 1], $professor_permissions);
             }
         }
-
+        $this->set(compact('professor_permissions'));
         $this->set(compact('professor_permissions_matrix'));
     }
 
@@ -197,31 +178,21 @@ class RolesController extends AppController
         $role_permissions = [];
         $this->loadModel('PermissionsRoles');
         $this->loadModel('Permissions');
-        //debug($module);
-        //debug($action);
         if ($role == 'Administrador') {
             $role_selected = 'administrator';
-            $role_permissions = $this->Permissions->find('list')->matching('Roles', function ($q) {
-                return $q->where(['Roles.role_id' => 'Administrador']);
-            })->toArray();
+            $role_permissions = $this->Permissions->getPermissions('Administrador');
 
         } else if ($role == 'Asistente') {
             $role_selected = 'assistant';
-            $role_permissions = $this->Permissions->find('list')->matching('Roles', function ($q) {
-                return $q->where(['Roles.role_id' => 'Asistente']);
-            })->toArray();
+            $role_permissions = $this->Permissions->getPermissions('Asistente');
 
         } else if ($role == 'Estudiante') {
             $role_selected = 'student';
-            $role_permissions = $this->Permissions->find('list')->matching('Roles', function ($q) {
-                return $q->where(['Roles.role_id' => 'Estudiante']);
-            })->toArray();
+            $role_permissions = $this->Permissions->getPermissions('Estudiante');
 
         } else if ($role== 'Profesor') {
             $role_selected = 'professor';
-            $role_permissions = $this->Permissions->find('list')->matching('Roles', function ($q) {
-                return $q->where(['Roles.role_id' => 'Profesor']);
-            })->toArray();
+            $role_permissions = $this->Permissions->getPermissions('Profesor');
         }
 
         return in_array($module.'-'.$action, $role_permissions);
