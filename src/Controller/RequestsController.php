83e9372218c04b2e7fde76a21cef9e4de1153998
Daniel <110100010111h@gmail.com>
2018-11-13 00:15:15 -0600

    Agrega validaciones a las solicitudes que llena el estudiante

--- src/Controller/RequestsController.php
+++ src/Controller/RequestsController.php
@@ -283,10 +283,13 @@ class RequestsController extends AppController
                 //Si el estudiante no marco ningun tipo de hora, entonces deja las horas asistente por defecto
                 $request->set('wants_assistant_hours',true);
             }
-            
-            //die();
+           
             //debug($request);
-            if ($this->Requests->save($request)) {
+            //die();
+            if($request['average'] < 7){
+                $this->Flash->error(__('Error: No se logró agregar la solicitud, su promedio es inferior a 7, por favor lea los requisitos'));
+                return $this->redirect(['controller'=>'Mainpage','action'=>'index']);
+            }else if ($this->Requests->save($request)) {
                 $this->Flash->success(__('Se agrego la Solicitud Correctamente'));
                 return $this->redirect(['action' => 'index']);
             }
